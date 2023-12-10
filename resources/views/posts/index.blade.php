28b8fe9d1618db41ccf91d87bc28507c8813a7e7
julinek999 <julnow01@gmail.com>
2023-12-10 18:47:31 +0000

    Added pagination to the posts index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,5 +7,6 @@
             <li><a href="/posts/{{ $post->id }}">{{ $post->post_body }}</a></li>
         @endforeach
     </ul>
+    {{ $posts->links() }}
     <a href=" {{ route('posts.create') }}">Create Post</a>
 @endsection
